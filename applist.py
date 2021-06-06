20f40252225d2a19ee49b354b22db8fc5fd88ee4
Rajdeep basu <48894311+17ack312@users.noreply.github.com>
2021-06-06 21:34:45 +0530

    Add files via upload

---
+++ applist.py
@@ -0,0 +1,23 @@
+import re
+import os
+
+os.system("apt update && apt upgrade -y")
+
+os.system("apt list >> /$HOME/list.txt")
+file="/$HOME/list.txt"
+f =open(file,"r")
+
+for i in f:
+    line=f.readline()
+    temp=re.split("/",line)
+   # print(temp[0])
+    string="apt install -y "+temp[0]+"\n"
+    g=open("/root/applist.txt","a")
+    g.write(string)
+
+q=input("\n[?]Do you want to view installer documentation ??(Y/n)\n[>] ").lower()
+
+if q=='y' or q=='yes':
+    os.system("cat /root/applist.txt")
+
+os.system("rm list.txt")
