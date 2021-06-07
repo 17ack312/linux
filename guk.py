639c6c24b5aa1d8ff5fa8bb5bd1a22f772bfd07a
Rajdeep basu <48894311+17ack312@users.noreply.github.com>
2021-06-08 00:50:29 +0530

    Update guk.py

--- guk.py
+++ guk.py
@@ -6,4 +6,4 @@ file=sys.argv[1]
 x=re.search("kali",file)
 if(bool(x)==True):
   os.system("apt update && apt upgrade && apt install kali-win-kex -y")
-
+print("[=]Enter KEX command to run KALI GUI")
